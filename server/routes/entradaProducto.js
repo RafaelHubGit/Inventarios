9d13f7281426eb27e67c4b43e27e6f23494d1650
GONZALO RAFAEL NAVA VARGAS <gonzalo.nava@senado.gob.mx>
2019-05-24 15:38:22 -0500

    CRUD Entrada

--- server/routes/entradaProducto.js
+++ server/routes/entradaProducto.js
@@ -5,6 +5,107 @@ let app = express();
 
 let Entrada = require('../models/entradaProducto');
 
+
+//=================================
+//Obtener los Entrada
+//=================================
+app.get( '/services/entrada', (req, res) => {
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 10;
+    limite = Number(limite);
+
+    Entrada.find({})
+        .skip(desde)
+        .limit(limite)
+        // .populate('categoria')
+        .exec( (err, entrada) => {
+            if( err ){
+                return res.json({
+                    ok: false, 
+                    err
+                });
+            };
+
+            Entrada.countDocuments( {}, (err, conteo) => {
+                res.json({
+                    ok: true, 
+                    entrada, 
+                    cuantos : conteo
+                });
+            });
+        });
+
+});
+
+
+//=================================
+//Obten Entrada por ID
+//=================================
+
+app.get('/services/entrada/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    Entrada.findById(id)
+        .exec( (err, entradaDB) => {
+            if( err ){
+                return res.status(500).json({
+                    ok: false, 
+                    err
+                });
+            };
+
+            if( !entradaDB ){
+                return res.status(400).json({
+                    ok: false, 
+                    message: 'La entrada no existe'
+                });
+            }
+
+            res.status(200).json({
+                ok: true, 
+                entrada: entradaDB
+            });
+
+        });
+
+
+});
+
+//========================
+//Buscar Entrada por nombre
+//========================
+app.get('/services/entrada/termino/:termino', (req, res) => {
+
+    let termino = req.params.termino; 
+
+    let regex = new RegExp(termino, 'i'); //La i es para que se insencible aminusculas y mayu
+
+    Entrada.find({ nombre: regex})
+        .exec( (err, entrada) => {
+            if( err ){
+                return res.status(500).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            Entrada.countDocuments( {nombre: regex}, (err, conteo) => {
+                res.json({
+                    ok: true, 
+                    entrada, 
+                    cuantos : conteo
+                });
+            });
+
+        })
+});
+
+
+
 //=================================
 //Crear un producto 
 //=================================
